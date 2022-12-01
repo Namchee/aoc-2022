3faaa70703a87c7b61f1e869b7957e7f81e8149e
Cristopher Namchee <cristopher@tokopedia.com>
2022-12-01 20:22:57 +0700

    feat: complete day one

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,9 +1,45 @@
+use std::cmp;
+
 pub fn solve_one(input: Vec<String>) -> String {
-    return  "hello world".to_string();
+    let mut cur: u32 = 0;
+    let mut best: u32 = 0;
+
+    for value in input.iter() {
+        if value.len() == 0 {
+            best = cmp::max(cur, best);
+            cur = 0;
+            continue;
+        }
+        let val = value.parse::<u32>().unwrap();
+        cur += val;
+    }
+
+    best = cmp::max(cur, best);
+
+    return format!("{}", best);
 }
 
 pub fn solve_two(input: Vec<String>) -> String {
-    return "goodbye ".to_string();
+    let mut cur: u32 = 0;
+    
+    let mut sum: Vec<u32> = vec![];
+
+    for value in input.iter() {
+        if value.len() == 0 {
+            sum.push(cur);
+            cur = 0;
+            continue;
+        }
+        let val = value.parse::<u32>().unwrap();
+        cur += val;
+    }
+
+    sum.push(cur);
+    sum.sort();
+
+    let result = sum[sum.len() - 1] + sum[sum.len() - 2] + sum[sum.len() - 3];
+
+    return format!("{}", result);
 }
 
 #[cfg(test)]
@@ -12,11 +48,44 @@ mod tests {
 
     #[test]
     fn test_solve_one() {
-        assert_eq!(solve_one(vec![]), "".to_string())
+        let str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        let input: Vec<String> = str.split("\n").map(|x| x.to_string()).collect();
+
+        assert_eq!(solve_one(input), "24000".to_string())
     }
 
     #[test]
     fn test_solve_two() {
-        assert_eq!(solve_two(vec![]), "".to_string())
+        let str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        let input: Vec<String> = str.split("\n").map(|x| x.to_string()).collect();
+        assert_eq!(solve_two(input), "45000".to_string())
     }
 }
